fb60e85b74fec63b3f076325779d73b8ab251093
robertfoltynowicz3526 <robert.foltynowicz3526@gmail.com>
2025-09-26 23:17:20 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA4VZWw2JvRR_IYFM-Ez75ZFtW3WodwTbc",
-  authDomain: "bobrzy-dashboard.firebaseapp.com",
-  projectId: "bobrzy-dashboard",
-  storageBucket: "bobrzy-dashboard.firebasestorage.app",
-  messagingSenderId: "648127119573",
-  appId: "1:648127119573:web:1c956817855adee82b625c",
-  measurementId: "G-9FPJ9DJEKM"
+  // TUTAJ WKLEJ SWOJE KLUCZE ZE STRONY FIREBASE
+  // Upewnij się, że są one takie same jak poprzednio.
+  apiKey: "AIzaSy...",
+  authDomain: "nazwa-twojego-projektu.firebaseapp.com",
+  projectId: "nazwa-twojego-projektu",
+  storageBucket: "nazwa-twojego-projektu.appspot.com",
+  messagingSenderId: "...",
+  appId: "..."
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// TA LINIJKA JEST NAJWAŻNIEJSZA - słowo "export" udostępnia `db` innym plikom
+export const db = getFirestore(app);
